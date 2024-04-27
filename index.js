d6a702ff3a16ef51523003a027d7bcb89ce7456d
Rk-Reaz <rkreaz91@gmail.com>
2024-04-28 02:06:21 +0600

    added Manage Items In deleted and Update menu

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
 
     // middleware 
     const verifyToken = (req, res, next) => {
-      console.log('Inside The Verify Token', req.headers.authorization);
+      // console.log('Inside The Verify Token', req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'Unauthorized Access' })
       }
@@ -60,7 +60,7 @@ async function run() {
           return res.status(401).send({ message: 'Unauthorized Access' })
         }
         req.decoded = decoded;
-        next()
+        next();
       })
 
     }
@@ -134,9 +134,47 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    })
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result)
+    })
+
+    app.delete('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      console.log("result", result);
+      res.send(result);
+    })
+
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedDoc)
+      res.send(result);
     })
 
     //cards collection API
